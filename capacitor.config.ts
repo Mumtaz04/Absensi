584d099fd41103fc435384625bd5e523ca17fa69
Mumtaz04 <mumtazjkt04@gmail.com>
2025-09-25 13:48:31 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Absensi',
+  webDir: 'www'
+};
+
+export default config;
